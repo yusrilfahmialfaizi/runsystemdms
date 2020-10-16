9e5c29ac2f71cc89f1b4f2313cec700c11663372
Yusufandi99 <44702492+Yusufandi99@users.noreply.github.com>
2020-10-16 09:37:17 +0700

    tampilan login

--- application/views/partials_login/parts/head.php
+++ application/views/partials_login/parts/head.php
@@ -1,5 +1,5 @@
 <head>
-    <title>Logdy - Login and Register HTML5 Template</title>
+    <title>Document Management System </title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="UTF-8">
     <!-- External CSS libraries -->
