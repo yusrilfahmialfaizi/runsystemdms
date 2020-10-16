106b54682e6364b5d5efea40378361e46628d5c0
Alex Rudi Herlambang <alexrudiherlambang98@gmail.com>
2020-10-16 14:12:45 +0700

    Update content_home.php

--- application/views/partials/content/content_home.php
+++ application/views/partials/content/content_home.php
@@ -6,71 +6,6 @@
                 </div>
             </div>
         </div>
-        <div class="social grid">
-            <div class="grid-info">
-                <div class="col-md-3 top-comment-grid">
-                    <div class="comments tweets">
-                        <div class="comments-icon">
-                            <i class="fa fa-twitter"></i>
-                        </div>
-                        <div class="comments-info tweets-info">
-                            <h3>05</h3>
-                            <a href="#">IMS 2</a>
-                        </div>
-                        <div class="clearfix"> </div>
-                    </div>
-                </div>
-                <div class="col-md-3 top-comment-grid">
-                    <div class="comments views">
-                        <div class="comments-icon">
-                            <i class="fa fa-eye"></i>
-                        </div>
-                        <div class="comments-info views-info">
-                            <h3>06</h3>
-                            <a href="#">Sarinah 1</a>
-                        </div>
-                        <div class="clearfix"> </div>
-                    </div>
-                </div>
-                <div class="col-md-3 top-comment-grid">
-                    <div class="comments views">
-                        <div class="comments-icon">
-                            <i class="fa fa-eye"></i>
-                        </div>
-                        <div class="comments-info views-info">
-                            <h3>07</h3>
-                            <a href="#">Sarinah 2</a>
-                        </div>
-                        <div class="clearfix"> </div>
-                    </div>
-                </div>
-                <div class="col-md-3 top-comment-grid">
-                    <div class="comments views">
-                        <div class="comments-icon">
-                            <i class="fa fa-eye"></i>
-                        </div>
-                        <div class="comments-info views-info">
-                            <h3>08</h3>
-                            <a href="#">Sarinah 3</a>
-                        </div>
-                        <div class="clearfix"> </div>
-                    </div>
-                </div>
-                <div class="col-md-3 top-comment-grid" style="margin:10px">
-                    <div class="comments views">
-                        <div class="comments-icon">
-                            <i class="fa fa-eye"></i>
-                        </div>
-                        <div class="comments-info views-info">
-                            <h3>08</h3>
-                            <a href="#">Sarinah 3</a>
-                        </div>
-                        <div class="clearfix"> </div>
-                    </div>
-                </div>
-                <div class="clearfix"> </div>
-            </div>
-        </div>
     </div>
     <?php $this->load->view("partials/parts/foot") ?>
     <script type="text/javascript">
@@ -85,7 +20,7 @@
                     data = JSON.parse(JSON.stringify(data));
                     data = data.pg;
                     for (i = 0; i < data.length; i++) {
-                        var grid = '<div class="col-md-3 top-comment-grid" style="margin : 5px"><div class="comments"><div class="comments-icon"><i class="fa fa-eye"></i></div><div class="comments-info"><h2 style="color:white;">' + data[i].pgcode + '</h2><a href="#">' + data[i].pgname + '</a></div><div class="clearfix"> </div></div></div>';
+                        var grid = '<div class="col-md-3 top-comment-grid" style="margin : 5px"><div class="comments tweets"><div class="comments-icon"><i class="fa fa-twitter"></i></div><div class="comments-info tweets-info"><h2 style="color:white;">' + data[i].pgcode + '</h2><a href="#">' + data[i].pgname + '</a></div><div class="clearfix"> </div></div></div>';
                         $("#grid").append(grid)
                     }
                 }
