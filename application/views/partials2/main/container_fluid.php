59e16ff50f0c3e4158ce1cc5164c3ddcccaf30d5
Yusril Fahmi Al Faizi <44188722+yusrilfahmialfaizi@users.noreply.github.com>
2020-10-26 14:08:15 +0700

    sidebar

--- application/views/partials2/main/container_fluid.php
+++ application/views/partials2/main/container_fluid.php
@@ -17,10 +17,15 @@
         data = data.pg;
         var grid = [];
         for (i = 0; i < data.length; i++){
-            grid.push('<div class="col-sm-6 col-xl-3 col-lg-6 box-col-6"><div class="card gradient-primary o-hidden"><div class="b-r-4 card-body"><div class="media static-top-widget"><div class="align-self-center text-center"><i data-feather="database"></i></div><div class="media-body"><span class="m-0 text-white">'+data[i].pgcode+'</span><h4 class="mb-0 counter">'+data[i].pgname+'</h4><i class="icon-bg" data-feather="database"></i></div></div></div></div></div>');
+            grid.push('<div class="col-sm-6 col-xl-3 col-lg-6 box-col-6"><div class="card gradient-primary o-hidden"><div class="b-r-4 card-body"><div class="media static-top-widget" onClick="project()"><div class="align-self-center text-center"><i data-feather="database"></i></div><div class="media-body"><span class="m-0 text-white">'+data[i].pgcode+'</span><h4 class="mb-0 counter">'+data[i].pgname+'</h4><i class="icon-bg" data-feather="database"></i></div></div></div></div></div>');
         }
         $("#grid").html(grid);
       }
     });
   });
+</script>
+<script type="text/javascript">
+    function project(){
+      $("#sidebar").removeClass("iconbar-second-close");
+    }
 </script>
--- application/views/partials2/main/sidebar/sidebar_admin.php
+++ application/views/partials2/main/sidebar/sidebar_admin.php
@@ -1,8 +1,8 @@
-<!-- Page Sidebar Start-->
-<div class="iconsidebar-menu">
-  <div class="sidebar">
-    <ul id="module" class="iconMenu-bar custom-scrollbar">
-    </ul>
-  </div>
-</div>
+        <!-- Page Sidebar Start-->
+        <div id="sidebar" class="iconsidebar-menu iconbar-second-close">
+          <div class="sidebar">
+            <ul id="module" class="iconMenu-bar custom-scrollbar">
+            </ul>
+          </div>
+        </div>
         <!-- Page Sidebar Ends-->
