d71617cf3e54d6a940ed1a3039a3449250147084
Yusril Fahmi Al Faizi <44188722+yusrilfahmialfaizi@users.noreply.github.com>
2020-11-04 16:00:20 +0700

    commit css

---
+++ application/views/partials2/main/head.php
@@ -0,0 +1,43 @@
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Poco admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
+    <meta name="keywords" content="admin template, Poco admin template, dashboard template, flat admin template, responsive admin template, web app">
+    <meta name="author" content="pixelstrap">
+    <link rel="shortcut icon" href="<?php echo base_url("assets/images/favicon.png") ?>" type="image/x-icon">
+    <link rel="icon" href="<?php echo base_url("assets/images/favicon.png") ?>" type="image/x-icon">
+    <title>Document Manajemen Sistem</title>
+    <!-- Google font-->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <!-- Font Awesome-->
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/fontawesome.css") ?>">
+    <!-- ico-font-->
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/icofont.css") ?>">
+    <!-- Themify icon-->
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/themify.css") ?>">
+    <!-- Flag icon-->
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/flag-icon.css") ?>">
+    <!-- Feather icon-->
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/feather-icon.css") ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/animate.css") ?>">
+    <!-- Plugins css start-->
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/chartist.css") ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/date-picker.css") ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/prism.css") ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/material-design-icon.css") ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/pe7-icon.css") ?>">
+
+    <script src="<?php echo base_url('assets/js/jquery2.0.3.min.js') ?>"></script>
+    <!-- Plugins css Ends-->
+    <!-- Plugins css start-->
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>/assets/css/datatables.css">
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>/assets/css/datatable-extension.css">
+    <!-- Bootstrap css-->
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/bootstrap.css") ?>">
+    <!-- App css-->
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/stylecss_fix.css") ?>">
+    <link id="color" rel="stylesheet" href="<?php echo base_url("assets/css/color-1.css") ?>" media="screen">
+    <!-- Responsive css-->
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/responsive.css") ?>">
+</head>
