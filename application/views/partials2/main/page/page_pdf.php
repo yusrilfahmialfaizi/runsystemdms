8bc9a85e82b68cce9be40baa7f93940303bd6970
Yusufandi99 <44702492+Yusufandi99@users.noreply.github.com>
2020-11-16 12:51:02 +0700

    pdf
    tampilan

---
+++ application/views/partials2/main/page/page_pdf.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <link rel="stylesheet" href="">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <style>
+    .line-title{
+      border: 0;
+      border-style: inset;
+      border-top: 1px solid #000;
+    }
+  </style>
+</head>
+<body>
+  <table style="width: 100%;">
+    <tr>
+      <td align="center">
+        <span style="line-height: 1.6; font-weight: bold;">
+          Document Management System
+          <br>Yogyakarta
+          <hr class="line-title"> 
+        </span>
+        <span  style="line-height: 1.2;">
+         <h4 align="left"> 1.1.Submodul
+          <br>Yogyakarta Document Management System </hp>
+        </span>
+        
+        </td>
+   
+    </tr>
+    
+</body>
+  
+</html>
