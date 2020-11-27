a51820068069c0d8daa6950eec69445828b87f7e
panggilrofan <seesaajeen2@gmail.com>
2020-11-27 14:58:43 +0700

    recode column active, date, status

--- application/views/partials2/main/content/admin/tabel.php
+++ application/views/partials2/main/content/admin/tabel.php
@@ -28,7 +28,7 @@
                 <thead>
                   <tr>
                     <th>Docno</th>
-                    <th>Active Ind</th>
+                    <th>Active Indicator</th>
                     <th>Status</th>
                     <th>Create By</th>
                     <th>Create Date</th>
@@ -43,16 +43,32 @@
                     <?php foreach ($get as $key) {
                       if ($key != null) {
                         # code...
-                        for ($i = 0; $i < count($key); $i++) { ?>
+                        for ($i = 0; $i < count($key); $i++) {
+                          $createdt = strtotime($key[$i]["createdt"]);
+                          $lastupdt = strtotime($key[$i]["lastupdt"]);
+                    ?>
 
                           <tr>
                             <td><?php echo $key[$i]["docno"]; ?></td>
-                            <td><?php echo $key[$i]["activeind"]; ?></td>
-                            <td><?php echo $key[$i]["status"]; ?></td>
+                            <td>
+                              <?php if ($key[$i]["activeind"] == "Y") {
+                                echo "Active";
+                              } else {
+                                echo "InActive";
+                              } ?>
+                            </td>
+                            <td><?php if ($key[$i]["status"] == "F") {
+                                  echo "Final";
+                                } else if ($key[$i]["status"] == "O") {
+                                  echo "Outstanding";
+                                } else {
+                                  echo "Cancel";
+                                }
+                                ?></td>
                             <td><?php echo $key[$i]["createby"]; ?></td>
-                            <td><?php echo $key[$i]["createdt"]; ?></td>
+                            <td><?php echo date("d-m-Y H:i", $createdt); ?></td>
                             <td><?php echo $key[$i]["lastupby"]; ?></td>
-                            <td><?php echo $key[$i]["lastupdt"]; ?></td>
+                            <td><?php echo date("d-m-Y H:i", $lastupdt); ?></td>
                             <td>
                               <button width="20" type="button" data-feather="edit" data-docno="<?php echo $key[$i]["docno"]; ?>" data-modulcode="<?php echo $key[$i]["modulcode"]; ?>" data-status="<?php echo $key[$i]["status"]; ?>" data-active="<?php echo $key[$i]["activeind"]; ?>" onClick="modules2(this)" data-id="<?php echo $key[$i]["modulname"]; ?>">Edit</button>
                               <button width="20" type="button" data-docno="<?php echo $key[$i]["docno"]; ?>" data-modulcode="<?php echo $key[$i]["modulcode"]; ?>" onClick="preview(this)" data-feather="book-open">Preview</button>
