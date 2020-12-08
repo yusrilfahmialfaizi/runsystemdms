38f4ff9d3d925d10f2f30e499fddcdbda13128da
panggilrofan <seesaajeen2@gmail.com>
2020-12-08 19:38:21 +0700

    nguanu

--- application/views/partials2/main/js/js_admin.php
+++ application/views/partials2/main/js/js_admin.php
@@ -1,22 +1,21 @@
 <!-- Bootstrap core JavaScript-->
-<script src="<?php echo base_url("assets_admin/vendor/jquery/jquery.min.js") ?>"></script>
-<script src="<?php echo base_url("assets_admin/vendor/bootstrap/js/bootstrap.bundle.min.js") ?>"></script>
+<script src="<?php echo base_url("assets_admin/assets/jquery/jquery.min.js") ?>"></script>
+<script src="<?php echo base_url("assets_admin/assets/bootstrap/js/bootstrap.bundle.min.js") ?>"></script>
 
 <!-- Core plugin JavaScript-->
-<script src="<?php echo base_url("assets_admin/vendor/jquery-easing/jquery.easing.min.js") ?>"></script>
+<script src="<?php echo base_url("assets_admin/assets/jquery-easing/jquery.easing.min.js") ?>"></script>
 
 <!-- Custom scripts for all pages-->
 <script src="<?php echo base_url("assets_admin/js/sb-admin-2.min.js") ?>"></script>
 
 <!-- Page level plugins -->
-<script src="<?php echo base_url("assets_admin/vendor/chart.js/Chart.min.js") ?>"></script>
+<script src="<?php echo base_url("assets_admin/assets/chart.js/Chart.min.js") ?>"></script>
 
 <!-- Page level custom scripts -->
 <script src="<?php echo base_url("assets_admin/js/demo/chart-area-demo.js") ?>"></script>
 <script src="<?php echo base_url("assets_admin/js/demo/chart-pie-demo.js") ?>"></script>
 <!-- Page level plugins -->
-<script src="<?php echo base_url("assets_admin/vendor/datatables/jquery.dataTables.min.js") ?>"></script>
-<script src="<?php echo base_url("assets_admin/vendor/datatables/dataTables.bootstrap4.min.js") ?>"></script>
+<script src="<?php echo base_url("assets_admin/assets/datatables/jquery.dataTables.min.js") ?>"></script>
+<script src="<?php echo base_url("assets_admin/assets/datatables/dataTables.bootstrap4.min.js") ?>"></script>
 <!-- Page level custom scripts -->
 <script src="<?php echo base_url("assets_admin/js/demo/datatables-demo.js") ?>"></script>
-    
